bf023f281da53a8115967d8f1e1a18b9c4c89a4b
Paulo Khouri <paulokhouri@gmail.com>
2013-04-05 19:53:23 -0300

    Initial Commit

---
+++ CameraApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CameraApp
+//
+//  Created by Paulo Khouri on 4/5/13.
+//  Copyright (c) 2013 Mobilitate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CameraApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CameraApp
+//
+//  Created by Paulo Khouri on 4/5/13.
+//  Copyright (c) 2013 Mobilitate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
